13d58752dd108b512107e5872a24c5f88bf67baa
Timo Stovermann <timo.stovermann@d-velop.de>
2021-12-01 16:12:24 +0100

    step

--- web/scripts/OverviewPage.ts
+++ web/scripts/OverviewPage.ts
@@ -1,9 +1,16 @@
+import {MDCDialog} from "@material/dialog/component";
+import {MDCTextField} from "@material/textfield/component";
+import SaveType from "./SaveType";
 
 document.addEventListener('DOMContentLoaded', () => {
     new OverviewPage();
 });
 
 class OverviewPage {
+
+    private _dialog: MDCDialog;
+    private _newTypeTF: MDCTextField;
+    private _newTypeBtn: HTMLButtonElement;
 
     constructor() {
 
@@ -12,7 +19,22 @@ class OverviewPage {
             el.addEventListener('click', ()=>{
                 console.log('hey');
                 (<any>window).lapi.NavigateToType(typeToEdit);
-            })
+            });
+            this._dialog = new MDCDialog(document.querySelector('.mdc-dialog'));
+
+            document.getElementById('createTypeBtn').addEventListener( 'click', ()=>{
+                this._dialog.open();
+            });
+
+            this._newTypeTF = new MDCTextField(this._dialog.root.querySelector('.mdc-text-field'));
+            this._newTypeBtn = this._dialog.root.querySelector('button:disabled');
+            this._newTypeTF.root.querySelector('input').addEventListener('change', ()=>{
+                this._newTypeBtn.disabled = this._newTypeTF.value === '';
+            });
+            this._newTypeBtn.addEventListener('click', ()=>{
+                SaveType(this._newTypeTF.value, [], []);
+                (<any>window).lapi.NavigateToType(this._newTypeTF.value);
+            });
         })
 
     }
