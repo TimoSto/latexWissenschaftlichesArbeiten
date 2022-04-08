19b1b78f9347e278566c2134db8f3fc73517e043
Timo Stovermann <timo.stovermann@d-velop.de>
2022-04-08 15:18:26 +0200

    template-files not in root and embedded

--- src/packages/domain/new_project.go
+++ src/packages/domain/new_project.go
@@ -1,11 +1,45 @@
 package domain
 
 import (
+	_ "embed"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
 )
+
+//go:embed templates/example.tex
+var texTemplate string
+
+//go:embed templates/styPackages/abk_verzeichnis.sty
+var abkSty string
+
+//go:embed templates/styPackages/anhang.sty
+var anhangSty string
+
+//go:embed templates/styPackages/codes.sty
+var codesSty string
+
+//go:embed templates/styPackages/fusszeilen.sty
+var fusszeileSty string
+
+//go:embed templates/styPackages/header_footer.sty
+var headerFooterSty string
+
+//go:embed templates/styPackages/inhaltsverzeichnis.sty
+var tocSty string
+
+//go:embed templates/styPackages/literatur.sty
+var literaturSty string
+
+//go:embed templates/styPackages/ueberschriften.sty
+var ueberschriftenSty string
+
+//go:embed templates/literatur_template.csv
+var literaturCSVTemplate string
+
+//go:embed templates/literature_types.json
+var literaturTypesTemplate string
 
 func CreateNewProject(name string) (bool, error) {
 	projects, err := GetProjects()
@@ -28,27 +62,27 @@ func CreateNewProject(name string) (bool, error) {
 	if err != nil {
 		return false, err
 	}
-	inputcsv, err := ioutil.ReadFile("literatur_template.csv")
+	//inputcsv, err := ioutil.ReadFile("literatur_template.csv")
+	//if err != nil {
+	//	return false, err
+	//}
+	err = ioutil.WriteFile("./projects/"+name+"/literatur.csv", []byte(literaturCSVTemplate), 0644)
 	if err != nil {
 		return false, err
 	}
-	err = ioutil.WriteFile("./projects/"+name+"/literatur.csv", inputcsv, 0644)
+	//inputljson, err := ioutil.ReadFile("literature_types.json")
+	//if err != nil {
+	//	return false, err
+	//}
+	err = ioutil.WriteFile("./projects/"+name+"/literature_types.json", []byte(literaturTypesTemplate), 0644)
 	if err != nil {
 		return false, err
 	}
-	inputljson, err := ioutil.ReadFile("literature_types.json")
-	if err != nil {
-		return false, err
-	}
-	err = ioutil.WriteFile("./projects/"+name+"/literature_types.json", inputljson, 0644)
-	if err != nil {
-		return false, err
-	}
-	inputtex, err := ioutil.ReadFile("example.tex")
-	if err != nil {
-		return false, err
-	}
-	err = ioutil.WriteFile("./projects/"+name+"/"+name+".tex", inputtex, 0644)
+	//inputtex, err := ioutil.ReadFile("example.tex")
+	//if err != nil {
+	//	return false, err
+	//}
+	err = ioutil.WriteFile("./projects/"+name+"/"+name+".tex", []byte(texTemplate), 0644)
 	if err != nil {
 		return false, err
 	}
@@ -66,10 +100,43 @@ func CreateNewProject(name string) (bool, error) {
 		return false, err
 	}
 
-	oldDir := "./styPackages"
-	newDir := fmt.Sprintf("./projects/%s/styPackages", name)
+	err = ioutil.WriteFile("./projects/"+name+"/styPackages/abk_verzeichnis.sty", []byte(abkSty), 0644)
+	if err != nil {
+		return false, err
+	}
+	err = ioutil.WriteFile("./projects/"+name+"/styPackages/anhang.sty", []byte(anhangSty), 0644)
+	if err != nil {
+		return false, err
+	}
+	err = ioutil.WriteFile("./projects/"+name+"/styPackages/codes.sty", []byte(codesSty), 0644)
+	if err != nil {
+		return false, err
+	}
+	err = ioutil.WriteFile("./projects/"+name+"/styPackages/fusszeilen.sty", []byte(fusszeileSty), 0644)
+	if err != nil {
+		return false, err
+	}
+	err = ioutil.WriteFile("./projects/"+name+"/styPackages/header_footer.sty", []byte(headerFooterSty), 0644)
+	if err != nil {
+		return false, err
+	}
+	err = ioutil.WriteFile("./projects/"+name+"/styPackages/inhaltsverzeichnis.sty", []byte(tocSty), 0644)
+	if err != nil {
+		return false, err
+	}
+	err = ioutil.WriteFile("./projects/"+name+"/styPackages/literatur.sty", []byte(literaturSty), 0644)
+	if err != nil {
+		return false, err
+	}
+	err = ioutil.WriteFile("./projects/"+name+"/styPackages/ueberschriften.sty", []byte(ueberschriftenSty), 0644)
+	if err != nil {
+		return false, err
+	}
 
-	err = CopyDir(oldDir, newDir)
+	//oldDir := "./styPackages"
+	//newDir := fmt.Sprintf("./projects/%s/styPackages", name)
+
+	//err = CopyDir(oldDir, newDir)
 
 	return false, err
 }
