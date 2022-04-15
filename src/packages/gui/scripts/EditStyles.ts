3be625703650a4ce1433011f9e91cef0c4833285
Timo Stovermann <timo.stovermann@d-velop.de>
2022-04-15 12:33:06 +0200

    option-select gui

--- src/packages/gui/scripts/EditStyles.ts
+++ src/packages/gui/scripts/EditStyles.ts
@@ -1,3 +1,5 @@
+import {MDCDialog} from "@material/dialog/component";
+import {MDCSelect} from "@material/select/component";
 
 
 document.addEventListener('DOMContentLoaded', ()=>{
@@ -17,8 +19,20 @@ class EditStyles {
     private _project: string;
     private _packages: Package[] = [];
 
+    private _optionDialog: MDCDialog;
+    private _optionSelect: MDCSelect;
+    private _optionsList: HTMLElement;
+    private _optionTmpl: HTMLElement;
+
     constructor() {
         this._project = new URLSearchParams(window.location.search).get('project')
+
+        this._optionDialog = new MDCDialog(document.querySelector('.mdc-dialog'));
+
+        this._optionSelect = new MDCSelect(document.querySelector('.mdc-select'));
+        this._optionTmpl = document.querySelector('.mdc-select li');
+        this._optionTmpl.remove();
+        this._optionsList = document.querySelector('.mdc-select ul');
 
         // this._includeButtons = Array.from(document.querySelectorAll('.include-icon'));
         // this._disableButtons = Array.from(document.querySelectorAll('.remove-icon'));
@@ -28,14 +42,15 @@ class EditStyles {
 
         lis.forEach((el, i) => {
             let newPackage = new Package();
-            newPackage.Name = el.getAttribute('data-package-name');
+            const pname = el.getAttribute('data-package-name');
+            newPackage.Name = pname;
             newPackage.Included = el.getAttribute('data-included');
             newPackage.ActiveOption = el.getAttribute('data-active-option');
             if( newPackage.ActiveOption.length == 0 ) {
                 newPackage.ActiveOption = undefined;
             }
             let avopt = el.getAttribute('data-available-options');
-            let avoptions = avopt.substr(1, avopt.length-2).split(' ');
+            const avoptions = avopt.substr(1, avopt.length-2).split(' ');
             if( avoptions.length > 0 && avoptions[0].length > 0 ) {
                 newPackage.AvailableOptions = avoptions;
             }
@@ -43,6 +58,7 @@ class EditStyles {
 
             const index = i;
             const element = el;
+            const activeOption = newPackage.ActiveOption;
 
             el.querySelector('.include-icon').addEventListener('click', ()=>{
                 this._packages[index].Included = 'true';
@@ -52,10 +68,28 @@ class EditStyles {
                 this._packages[index].Included = 'false';
                 element.setAttribute('data-included', 'false');
             });
+
+            const editBtn = el.querySelector('.edit-icon');
+            if( editBtn ) {
+                editBtn.addEventListener('click', ()=>{
+
+                    this.openOptionDialog(pname, avoptions, activeOption);
+                });
+            }
         });
+    }
 
-
-
-        console.log(this._packages);
+    private openOptionDialog(pckg: string, options: string[], selected: string) {
+        document.querySelector('#packagename').innerHTML = pckg;
+        this._optionsList.innerHTML = '';
+        options.forEach(option => {
+            let li = <HTMLElement>this._optionTmpl.cloneNode(true);
+            li.querySelector('.mdc-deprecated-list-item__text').innerHTML = option;
+            li.setAttribute('data-value', option);
+            this._optionsList.appendChild(li);
+        });
+        this._optionSelect.layoutOptions();
+        this._optionSelect.layout();
+        this._optionDialog.open();
     }
 }
