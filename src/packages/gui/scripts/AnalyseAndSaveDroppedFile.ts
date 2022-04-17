90cb39b99464213d31c156b36336ac12aa25152d
Timo Stovermann <timo.stovermann@d-velop.de>
2022-04-17 20:13:52 +0200

    fileupload with loop works on single uploads

--- src/packages/gui/scripts/AnalyseAndSaveDroppedFile.ts
+++ src/packages/gui/scripts/AnalyseAndSaveDroppedFile.ts
@@ -7,101 +7,127 @@ export default function AnalyseAndSaveDroppdFile(file: String, project: string) 
         (<any>window.parent).openErrorDialog('Die Datei hat ein ungültiges Format.')
         return
     }
-    //remove stuff before first '@'
-    file = file.substr(file.indexOf('@'));
 
     //remove all linebreaks to have consistent format over all exports (Springer vs. IEEE vs. ...)
     file = file.replaceAll('\n', '');
     file = file.replaceAll('\r', '');
 
-    //type = string between first '@' and first '{'
-    let type = file.substring(1, file.indexOf('{')).toLowerCase();
-    //remove type and surrounding '{}'
-    file = file.substring(file.indexOf('{') + 1, file.lastIndexOf('}'))
+    while( file.indexOf('@') >= 0 ) {
+        //remove stuff before first '@'
+        file = file.substr(file.indexOf('@'));
 
-    //key ist string before first ',#
-    let key = file.substring(0, file.indexOf(','));
-    file = file.substring(file.indexOf(',') + 1);
+        console.log(file)
 
-    //parsing file to valuepairs
-    let valuepairs = [];
+        let nextEntryIndex = file.substr(1).indexOf('@');
 
-    while( file.length > 0 && valuepairs.length < 30 ) {
-        //attr-name ist string before '=' without leading or trailing spaces
-        let attr = file.substring(0, file.indexOf('='))
-        while( attr.charAt(0) === ' ' ) {
-            attr = attr.substring(1);
+        console.log(nextEntryIndex)
+
+        let filepart = file
+
+        if( nextEntryIndex >= 0) {
+            filepart = file.substr(0, nextEntryIndex)
         }
-        while( attr.charAt(attr.length-1) === ' ' ) {
-            attr = attr.substring(0, attr.length - 1);
-        }
-        file = file.substring(file.indexOf('=') + 1);
 
-        //to find value: string until first comma when all brackets are closed
-        let commaIndex;
-        let openBrackets = 0;
-        for( let i = 0 ; i < file.length && !commaIndex ; i++ ) {
-            if( file.charAt(i) === ',' && openBrackets == 0 || i === file.length-1) {
-                commaIndex = i;
-            } else if( file.charAt(i) === '{' ) {
-                openBrackets ++;
-            } else if( file.charAt(i) === '}' ) {
-                openBrackets --;
+        //type = string between first '@' and first '{'
+        let type = filepart.substring(1, filepart.indexOf('{')).toLowerCase();
+        //remove type and surrounding '{}'
+        filepart = filepart.substring(file.indexOf('{') + 1, filepart.lastIndexOf('}'))
+
+        //key ist string before first ',#
+        let key = filepart.substring(0, filepart.indexOf(','));
+        filepart = filepart.substring(filepart.indexOf(',') + 1);
+
+        //parsing file to valuepairs
+        let valuepairs = [];
+
+        while( filepart.length > 0 && valuepairs.length < 30 ) {
+            //attr-name ist string before '=' without leading or trailing spaces
+            let attr = filepart.substring(0, filepart.indexOf('='))
+            while( attr.charAt(0) === ' ' ) {
+                attr = attr.substring(1);
+            }
+            while( attr.charAt(attr.length-1) === ' ' ) {
+                attr = attr.substring(0, attr.length - 1);
+            }
+            filepart = filepart.substring(filepart.indexOf('=') + 1);
+
+            //to find value: string until first comma when all brackets are closed
+            let commaIndex;
+            let openBrackets = 0;
+            for( let i = 0 ; i < filepart.length && !commaIndex ; i++ ) {
+                if( filepart.charAt(i) === ',' && openBrackets == 0 || i === filepart.length-1) {
+                    commaIndex = i;
+                } else if( filepart.charAt(i) === '{' ) {
+                    openBrackets ++;
+                } else if( filepart.charAt(i) === '}' ) {
+                    openBrackets --;
+                }
+            }
+            let value = filepart.substring(0,commaIndex);
+            //remove possible leading and trailing spaces
+            if( value.charAt(0) === ' ' ) {
+                value = value.substring(1);
+            }
+            if( value.charAt(value.length-1) === ' ' ) {
+                value = value.substring(0, value.length - 1);
+            }
+            //remove possible leading and trailing brackets
+            while( value.charAt(0) === '{' ) {
+                value = value.substring(1);
+            }
+            while( value.charAt(value.length-1) === '}' ) {
+                value = value.substring(0, value.length - 1);
+            }
+
+            filepart = filepart.substring(commaIndex + 1)
+
+            value = ParseBibToString(value);
+
+            value = ParseStringToTeX(value);
+
+            console.log(attr, value)
+
+            valuepairs.push([attr, value])
+        }
+
+        if( AttrExists(valuepairs, 'doi') ) {
+            switch (type) {
+                case "article":
+                    type = "citaviAufsatzDoi"
+                    break
+                case "inbook":
+                    type = "citaviInbookDoi"
+                    break
+                case "inproceedings":
+                    type = "citaviInProceedingsDoi"
+                    break
+                case "incollection":
+                    type = "citaviInCollectionDoi"
+                    break
             }
         }
-        let value = file.substring(0,commaIndex);
-        //remove possible leading and trailing spaces
-        if( value.charAt(0) === ' ' ) {
-            value = value.substring(1);
-        }
-        if( value.charAt(value.length-1) === ' ' ) {
-            value = value.substring(0, value.length - 1);
-        }
-        //remove possible leading and trailing brackets
-        while( value.charAt(0) === '{' ) {
-            value = value.substring(1);
-        }
-        while( value.charAt(value.length-1) === '}' ) {
-            value = value.substring(0, value.length - 1);
-        }
 
-        file = file.substring(commaIndex + 1)
+        let sortedvaluepairs = SortValues(valuepairs, type);
 
-        value = ParseBibToString(value);
 
-        value = ParseStringToTeX(value);
-
-        valuepairs.push([attr, value])
-    }
-
-    if( AttrExists(valuepairs, 'doi') ) {
-        switch (type) {
-            case "article":
-                type = "citaviAufsatzDoi"
-                break
-            case "inbook":
-                type = "citaviInbookDoi"
-                break
-            case "inproceedings":
-                type = "citaviInProceedingsDoi"
-                break
-            case "incollection":
-                type = "citaviInCollectionDoi"
-                break
-        }
-    }
-
-    let sortedvaluepairs = SortValues(valuepairs, type);
-
-    SaveEntry('', project, sortedvaluepairs, type, key, '').then(valid => {
-        if (valid) {
-
-            (<any>window.parent).reloadMain();
-            (<any>window.parent).setEdit('/editEntry?project='+project+'&entry='+key);
+        if( nextEntryIndex >= 0 ) {
+            file = file.substr(nextEntryIndex);
         } else {
-            (<any>window.parent).openErrorDialog('Beim Versuch, die Citavi-Quelle hochzuladen, ist ein Fehler aufgetreten.')
+            file = '';
         }
-    });
+
+        console.log(sortedvaluepairs)
+
+        SaveEntry('', project, sortedvaluepairs, type, key, '').then(valid => {
+            if (valid) {
+
+                (<any>window.parent).reloadMain();
+                (<any>window.parent).setEdit('/editEntry?project='+project+'&entry='+key);
+            } else {
+                (<any>window.parent).openErrorDialog('Beim Versuch, die Citavi-Quelle hochzuladen, ist ein Fehler aufgetreten.')
+            }
+        });
+    }
 }
 
 function SortValues(valuepairs: string[][], type: string) {
