227131389d58550b52aeaea668109b137207c792
Timo Stovermann <timo.stovermann@d-velop.de>
2022-05-27 19:58:32 +0200

    parsing comments works

--- src/packages/gui/scripts/Editor.ts
+++ src/packages/gui/scripts/Editor.ts
@@ -27,11 +27,37 @@ class Editor {
     private parseContent() {
         let file = this.editArea.innerText;
 
-        let commentResults = file.match(/^(?:%).*/gm)
+        let lastCommentStart = -1;
+        let lastNewline = -1;
+        let lastBackslash = -1;
+        let lastOpenBrace = -1;
 
-        commentResults.forEach(res => {
-            file = file.replace(res,'<span class="command comment">'+res+'</span>')
-        });
+        let inComment = false;
+        let inCommand = false;
+
+        let openBraces = 0;
+        let openSquareBrackets = 0;
+
+        let commentPrefix = '<span class="command comment">'
+
+        let commandSuffix = '</span>'
+
+        for( let i = 0 ; i < file.length ; i++ ) {
+            if( file.charAt(i) == `\\` ) {
+                lastBackslash = i;
+            } else if ( file.charAt(i) == `%` ) {
+                if( !(lastBackslash >= 0 && lastBackslash == i-1) && file.charAt(i-1) !== `>` ) {
+                    lastCommentStart = i;
+                    inComment = true;
+                }
+            } else if ( file.charAt(i) == `\n` ) {
+                if( inComment ) {
+                    file = file.substring(0, lastCommentStart) + commentPrefix + file.substring(lastCommentStart, i) + commandSuffix + file.substring(i);
+                    inComment = false;
+                    i += commentPrefix.length + commandSuffix.length;
+                }
+            }
+        }
 
         this.editArea.innerHTML = file;
     }
