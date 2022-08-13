393c719dfa52d293ae668214749f98e1bf6edf58
Timo Stovermann <tsto@d-velop.de>
2022-08-13 15:06:57 +0200

    move

--- src/packages/webclient/src/App.vue
+++ src/packages/webclient/src/App.vue
@@ -2,21 +2,11 @@
   <v-app>
     <v-app-bar app color="primary" dark clipped-left>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>Title</v-toolbar-title>
+      <v-toolbar-title>TeXThesor</v-toolbar-title>
     </v-app-bar>
 
     <v-navigation-drawer app v-model="drawer" permanent :mini-variant.sync="drawer" clipped>
-      <v-list nav>
-        <v-list-item v-for="item in items" :key="item.title" link>
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
 
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
     </v-navigation-drawer>
 
     <v-main> </v-main>
@@ -30,10 +20,7 @@ export default Vue.extend({
   name: 'App',
 
   data: () => ({
-    drawer: false,
-    items: [
-      {title: 'test', icon: 'mdi-image'}
-    ]
+    drawer: false
   }),
 });
 </script>
