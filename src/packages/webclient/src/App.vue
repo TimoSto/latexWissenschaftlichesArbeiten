90b4476267e7b5322a58c4521de6ab402771602e
Timo Stovermann <tsto@d-velop.de>
2022-08-13 15:27:22 +0200

    toggle dark and light

--- src/packages/webclient/src/App.vue
+++ src/packages/webclient/src/App.vue
@@ -3,6 +3,8 @@
     <v-app-bar app color="primary" dark clipped-left>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>TeXThesor</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon @click="toggleDark"><v-icon>mdi-brightness-6</v-icon></v-btn>
     </v-app-bar>
 
     <v-navigation-drawer app v-model="drawer" permanent :mini-variant.sync="drawer" clipped>
@@ -22,6 +24,11 @@ export default Vue.extend({
   data: () => ({
     drawer: false
   }),
+  methods: {
+    toggleDark () {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+    }
+  }
 });
 </script>
 <style lang="scss">
--- src/packages/webclient/src/plugins/vuetify.ts
+++ src/packages/webclient/src/plugins/vuetify.ts
@@ -1,7 +1,27 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: '#1976D2',
+                secondary: '#333E48',
+                accent: '#6da6dc',
+                error: '#b00020',
+            },
+            dark: {
+                primary: '#1a679c',
+                secondary: '#8D9BA8',
+                accent: '#3a5770',
+                error: '#CF6679'
+            }
+        },
+    },
+    icons: {
+        iconfont: 'mdi',
+    },
 });
