23457ab486a9727218d367b658f93c60be989e44
Timo Stovermann <tsto@d-velop.de>
2022-08-13 14:56:40 +0200

    minifiable drawer

--- webclient/src/App.vue
+++ webclient/src/App.vue
@@ -1,45 +1,25 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+    <v-app-bar app color="primary" dark clipped-left>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Title</v-toolbar-title>
     </v-app-bar>
 
-    <v-main>
-      <router-view/>
-    </v-main>
+    <v-navigation-drawer app v-model="drawer" permanent :mini-variant.sync="drawer" clipped>
+      <v-list nav>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main> </v-main>
   </v-app>
 </template>
 
@@ -50,7 +30,10 @@ export default Vue.extend({
   name: 'App',
 
   data: () => ({
-    //
+    drawer: false,
+    items: [
+      {title: 'test', icon: 'mdi-image'}
+    ]
   }),
 });
 </script>
