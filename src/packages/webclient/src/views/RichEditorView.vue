c397522156a5b13246158f805feb7888a68e26de
Timo Stovermann <tsto@d-velop.de>
2022-09-03 20:47:31 +0200

    basic setup with banner

--- src/packages/webclient/src/views/RichEditorView.vue
+++ src/packages/webclient/src/views/RichEditorView.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
-    <v-banner>
-      Die Editoren befinden sich noch in der Beta-Phase. Eine saubere Funktionalität ist nicht garantiert!
-      <template v-slot:actions="{ dismiss }">
-        <v-btn
-            text
-            color="primary"
-            @click="dismiss"
-        >
-          Verstanden
-        </v-btn>
-      </template>
-    </v-banner>
+    <v-app-bar elevate-on-scroll color="background" scroll-target="#scrollr" style="z-index: 100">
+      <v-toolbar-title>Editor</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+    </v-app-bar>
+    <v-sheet style="overflow-y: scroll; height: calc(100vh - 130px); padding: 0" id="scrollr">
+      <v-banner sticky>
+        <v-icon :style="[ this.$vuetify.theme.dark ? {'color': 'rgba(255,221,0,1)'} : {'color': 'rgba(255,202,10,1)'} ]" slot="icon" size="36">mdi-alert</v-icon>
+        Die Editoren befinden sich noch in der Beta-Phase. Eine saubere Funktionalität ist nicht garantiert!
+        <template v-slot:actions="{ dismiss }">
+          <v-btn
+              text
+              color="primary"
+              @click="dismiss"
+          >
+            Verstanden
+          </v-btn>
+        </template>
+      </v-banner>
+      hallo
+    </v-sheet>
   </div>
 </template>
 
@@ -23,6 +32,6 @@ export default Vue.extend({
 })
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+@import "../styles/fixesForDark.scss";
 </style>
