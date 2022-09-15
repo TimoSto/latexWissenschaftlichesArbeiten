99cf33c6dbeb0ce6622e32c90cae6c7ed95df437
Timo Stovermann <tsto@d-velop.de>
2022-09-15 16:55:22 +0200

    fix

--- webclient/vue.config.js
+++ webclient/vue.config.js
@@ -2,5 +2,17 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    proxy: {
+      '/getPDF': {
+        target: 'http://localhost:8081/getPDF'
+      },
+      '/': {
+        target: 'http://localhost:8081/',
+        ws: false
+      }
+    }
+  },
+  outputDir: './assets/dist'
 })
