5c4f35385a39cce0cfb80aa7f461b960a9daafc3
Timo Stovermann <tsto@d-velop.de>
2022-09-17 13:09:34 +0200

    tests

---
+++ internal/bib_entries/count_cites_test.go
@@ -0,0 +1,29 @@
+package bib_entries
+
+import (
+	"testing"
+
+	"WA_LaTeX/internal/mockFileSystem"
+)
+
+func TestCountCites_empty(t *testing.T) {
+	entries, err := ReadBibEntries("test", mockFileSystem.ReadFile)
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+	for _, e := range entries {
+		if e.CiteNumber != 0 {
+			t.Errorf("expected 0 cites for entry %s, got %v", e.Key, e.CiteNumber)
+		}
+	}
+}
+
+func TestCountCites_one(t *testing.T) {
+	entries, err := ReadBibEntries("test2", mockFileSystem.ReadFile)
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+	if entries[0].CiteNumber != 1 {
+		t.Errorf("expected 1 cites for entry %s, got %v", entries[0].Key, entries[0].CiteNumber)
+	}
+}
