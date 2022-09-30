8449e27e2691252e9a8b60916526be2837b56bef
Timo Stovermann <tsto@d-velop.de>
2022-09-30 15:38:08 +0200

    drawer setup

--- webclient/src/App.vue
+++ webclient/src/App.vue
@@ -8,7 +8,7 @@
       fixed
     >
 
-      <v-app-bar-nav-icon />
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
 
       <v-toolbar-title>ThesorTeX</v-toolbar-title>
 
@@ -19,6 +19,23 @@
       </v-btn>
 
     </v-app-bar>
+
+    <v-navigation-drawer app v-model="drawer" permanent :mini-variant="drawer" clipped>
+      <v-app-bar elevation="0" color="background">
+        <v-toolbar-title v-if="!drawer">Projekte</v-toolbar-title>
+        <v-spacer v-if="!drawer"></v-spacer>
+        <v-btn icon :to="'/new'" title="Neues Projekt erstellen">
+          <v-icon>mdi-plus</v-icon>
+        </v-btn>
+      </v-app-bar>
+      <v-sheet style="overflow-y: auto; height: calc(100vh - 130px); padding: 0; background-color: var(--v-background-base)" id="scrollsidebar">
+        <v-list class="keep">
+          <v-list-item v-for="proj in projectNames" :key="proj">
+            <v-list-item-title v-text="proj"></v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-sheet>
+    </v-navigation-drawer>
 
     <v-main>
       <router-view/>
@@ -33,15 +50,22 @@ export default Vue.extend({
   name: 'App',
 
   data: () => ({
-    //
+    drawer: false
   }),
 
   methods: {
-
     toggleDark() {
       this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
     }
+  },
+
+
+  computed: {
+    projectNames(): string[] {
+      return []
+    }
   }
+
 });
 </script>
 
