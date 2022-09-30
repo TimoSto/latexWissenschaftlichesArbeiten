8ab7b0b5110034d8d674847f6208da7d5300da93
Timo Stovermann <tsto@d-velop.de>
2022-09-30 20:22:42 +0200

    sidebar elevateonscroll

--- webclient/src/App.vue
+++ webclient/src/App.vue
@@ -21,14 +21,14 @@
     </v-app-bar>
 
     <v-navigation-drawer app v-model="drawer" permanent :mini-variant="drawer" clipped>
-      <v-app-bar elevation="0" color="background">
+      <v-app-bar elevation="1" color="background" elevate-on-scroll scroll-target="#scroll-sidebar">
         <v-toolbar-title v-if="!drawer">Projekte</v-toolbar-title>
         <v-spacer v-if="!drawer"></v-spacer>
         <v-btn icon :to="'/new'" title="Neues Projekt erstellen">
           <v-icon>mdi-plus</v-icon>
         </v-btn>
       </v-app-bar>
-      <v-sheet style="overflow-y: auto; height: calc(100vh - 130px); padding: 0; background-color: var(--v-background-base)" id="scrollsidebar">
+      <v-sheet style="overflow-y: auto; height: calc(100vh - 130px); padding: 0; background-color: var(--v-background-base)" id="scroll-sidebar">
         <v-list class="keep">
           <v-list-item v-for="proj in projectNames" :key="proj">
             <v-list-item-title v-text="proj"></v-list-item-title>
