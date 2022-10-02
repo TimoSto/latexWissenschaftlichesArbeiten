ff07ef8b95557e0c4cd6c87ae06e6de35fd27cf2
Timo Stovermann <tsto@d-velop.de>
2022-10-02 16:35:09 +0200

    citavi-area

--- webclient/src/views/TypeEditorView.vue
+++ webclient/src/views/TypeEditorView.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <v-app-bar dense color="background" elecation="1" elevate-on-scroll>
-      <v-toolbar-title>Literaturtyp</v-toolbar-title>
+    <v-app-bar dense color="background" elecation="1" elevate-on-scroll scroll-target="#scroll-editor">
+      <v-toolbar-title>Literaturtyp: <i>{{$store.state.editor.key}}</i></v-toolbar-title>
 
       <v-spacer />
 
@@ -9,10 +9,109 @@
         <span v-html="layoutBtnContent[0]" style="color: var(--v-accent-lighten2)"></span>
       </v-btn>
     </v-app-bar>
+
+    <v-sheet id="scroll-editor" class="content-below-two-bars">
+
+      <v-expansion-panels multiple accordion flat tile>
+
+        <v-expansion-panel>
+          <v-expansion-panel-header>Allgemein</v-expansion-panel-header>
+          <v-expansion-panel-content>
+
+            <v-simple-table disable-sort dense class="two-col-table">
+              <tbody>
+                <tr>
+                  <td>
+                    Bezeichnung
+                  </td>
+                  <td style="text-align: right">
+                    <v-text-field type="string" v-model="$store.state.editor.typeToEdit.Name" :rules="nameRules"/>
+                  </td>
+                </tr>
+              </tbody>
+            </v-simple-table>
+
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+
+        <v-expansion-panel>
+          <v-expansion-panel-header>Citavi-Import</v-expansion-panel-header>
+          <v-expansion-panel-content>
+
+            <v-simple-table disable-sort dense class="two-col-table">
+              <tbody>
+              <tr>
+                <td>
+                  Citavi-Typ
+                </td>
+                <td>
+                  <v-text-field
+                      :prefix="citaviPrefix"
+                      placeholder="Citavi-Typ"
+                      type="string"
+                      v-model="$store.state.editor.typeToEdit.CitaviType"
+                      @blur="citaviPrefix = ($store.state.editor.typeToEdit.CitaviType && $store.state.editor.typeToEdit.CitaviType.length > 0) ? '@' : ''"
+                  />
+                </td>
+              </tr>
+
+              <tr>
+                <td>
+                  Citavi-Pflichtfelder
+                </td>
+                <td>
+                  <v-combobox
+                      v-model="$store.state.editor.typeToEdit.CitaviNecessaryFields"
+                      multiple
+                      :items="['doi', 'url']"
+                      @focus="focusedComboCitavi=true"
+                      @blur="focusedComboCitavi=false"
+                  >
+                    <!--to have no checkbox-->
+                    <template v-slot:item="{ item }">
+                      {{item}}
+                    </template>
+                    <!--to only show 4 in unfocused state -->
+                    <template v-slot:selection="{ item, index }" v-if="!focusedComboCitavi">
+                      <span v-if="index < 4">{{ item }} &nbsp;</span>
+                      <span
+                          v-if="index === 4"
+                          class="grey--text caption"
+                      >(+{{ $store.state.editor.typeToEdit.CitaviNecessaryFields.length - 4 }})</span>
+                    </template>
+
+                  </v-combobox>
+                </td>
+              </tr>
+
+              </tbody>
+            </v-simple-table>
+
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+
+        <v-expansion-panel>
+          <v-expansion-panel-header>Literaturverzeichnis</v-expansion-panel-header>
+          <v-expansion-panel-content>
+
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+
+        <v-expansion-panel>
+          <v-expansion-panel-header>Zitate</v-expansion-panel-header>
+          <v-expansion-panel-content>
+
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+
+      </v-expansion-panels>
+
+    </v-sheet>
+
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import Vue from "vue";
 
 export default Vue.extend({
@@ -20,6 +119,16 @@ export default Vue.extend({
   props: [
       'projectName'
   ],
+
+  data() {
+    return {
+      nameRules: [
+        (value: any) => !!value || 'Pflichtfeld',
+      ],
+      citaviPrefix: '',
+      focusedComboCitavi: false
+    }
+  },
 
   computed: {
     layoutBtnContent() {
@@ -38,6 +147,16 @@ export default Vue.extend({
 })
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.two-col-table {
+  max-width: 450px;
+  &.theme--light {
+    //border-bottom: 1px solid rgba(128,128,128, 0.25);
+  }
+  & tr {
+    & td:first-child {
+      width: 150px;
+    }
+  }
+}
 </style>
