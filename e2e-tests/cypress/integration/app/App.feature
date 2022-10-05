ddee1bac9db1a229ff338a3369736dff09c2aab3
Timo Stovermann <tsto@d-velop.de>
2022-10-05 14:02:46 +0200

    fist tests

--- e2e-tests/cypress/integration/app/App.feature
+++ e2e-tests/cypress/integration/app/App.feature
@@ -3,3 +3,12 @@
     
     Szenario: App laden
       Angenommen die App wurde mit dem Hash '#/' aufgerufen
+      Dann wird die Startseite mit 4 Cards angezeigt
+      Und sind 0 btns in der toolbar
+
+    Szenario: Wechsel zu documentation-projekt
+      Angenommen die App wurde mit dem Hash '#/' aufgerufen
+      Dann existiert ein Projekt mit dem Namen 'documentation'
+      Wenn zum Projekt 'documentation' gewechselt wird
+      Dann sind 3 btns in der toolbar
+      Und ist der Titel des mittleren Bereiches 'documentation'
--- e2e-tests/cypress/support/step_definitions/basics.js
+++ e2e-tests/cypress/support/step_definitions/basics.js
@@ -1,4 +1,24 @@
 
 Given('die App wurde mit dem Hash {string} aufgerufen', (hash) => {
     cy.visit('http://localhost:8080/' + hash)
+});
+
+Then('wird die Startseite mit {int} Cards angezeigt', ()=> {
+    cy.get('.v-card').should('have.length', 4)
+});
+
+Then('sind {int} btns in der toolbar', (n)=> {
+    cy.get('main').find('header').find('.v-btn').should('have.length', n)
+})
+
+Then('existiert ein Projekt mit dem Namen {string}', (name) => {
+    cy.get('nav').find('.v-list-item').contains(name).should('exist').should('have.length', 1)
+})
+
+When('zum Projekt {string} gewechselt wird', (name) => {
+    cy.get('nav').find('.v-list-item').contains(name).click();
+})
+
+Then('ist der Titel des mittleren Bereiches {string}', (name) => {
+    cy.get('main').find('#pages').find('header').contains(name).should('exist').should('be.visible')
 })
