08fb5369a81b8b14c2b3795e55ac73c3ca00dd0c
Timo Stovermann <tsto@d-velop.de>
2022-10-05 14:24:18 +0200

    splitting file at @

--- webclient/src/api/citavi/AnalyseDroppedFile.ts
+++ webclient/src/api/citavi/AnalyseDroppedFile.ts
@@ -1,5 +1,10 @@
 import {BibType} from "@/api/bibType/BibType";
 
 export default function AnalyseDroppedFile(file: string, types: BibType[]) {
-    console.log('analyse')
+    console.log('analyse', file)
+
+    //step 1: split att @ at beginning of line
+    const entries = file.split(/^@/gm).filter(e => e.length > 0);
+
+
 }
