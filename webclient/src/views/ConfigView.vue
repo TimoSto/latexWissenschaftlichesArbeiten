9663ad3dff000428c2278ce3a46ae70773f1f00f
Timo Stovermann <tsto@d-velop.de>
2022-10-09 16:43:56 +0200

    empty config view

---
+++ webclient/src/views/ConfigView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-app-bar dense color="background" elevation="1" elevate-on-scroll scroll-target="#scroll-config" style="z-index: 100">
+
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ConfigView"
+}
+</script>
+
+<style scoped>
+
+</style>
