aee0db1c31aef5746753efab8dd2a69b957b1885
Timo Stovermann <tsto@d-velop.de>
2022-10-10 13:18:52 +0200

    add area

--- webclient/src/views/ConfigView.vue
+++ webclient/src/views/ConfigView.vue
@@ -3,6 +3,8 @@
     <v-app-bar dense color="background" elevation="1" elevate-on-scroll scroll-target="#scroll-config" style="z-index: 100">
       <v-toolbar-title>{{$t(i18nDictionary.CONFIG_VIEW)}}</v-toolbar-title>
     </v-app-bar>
+    <v-sheet id="scroll-config" class="content-below-two-bars">
+    </v-sheet>
   </div>
 </template>
 
