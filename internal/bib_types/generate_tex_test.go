2f69dcd269058be8c0bf8e342e4d205f388a230c
Timo Stovermann <tsto@d-velop.de>
2022-10-10 18:22:17 +0200

    test generate bib-command

--- internal/bib_types/generate_tex_test.go
+++ internal/bib_types/generate_tex_test.go
@@ -33,3 +33,38 @@ func TestGenerateIfsForBib(t *testing.T) {
 		t.Errorf("expected \t\t\t{test}{printtest{#2}{#3}} not %v", ifs)
 	}
 }
+
+func TestGeneratePrintBibCommands(t *testing.T) {
+	types := []LiteratureType{
+		{
+			Name: "test",
+			Fields: []Field{
+				{
+					Field:            "test",
+					Style:            "italic",
+					Prefix:           "",
+					Suffix:           ", ",
+					TexValue:         false,
+					CitaviAttributes: nil,
+				},
+				{
+					Field:            "test2",
+					Style:            "normal",
+					Prefix:           "",
+					Suffix:           ".",
+					TexValue:         false,
+					CitaviAttributes: nil,
+				},
+			},
+		},
+	}
+
+	commands := GeneratePrintBibCommands(types)
+
+	expected := "\\newcommand{\\printtest}[2]{\n\t\\hangindent=\\bibparindent\n\t\\parindent 0pt\n\t\\hangafter=1\n\t\\textit{#1}, #2.\n\t\\\\\n\t\\vspace{-12pt}\n\n}\n"
+
+	if commands != expected {
+		t.Errorf("exxpected %s got %s", expected, commands)
+	}
+
+}
