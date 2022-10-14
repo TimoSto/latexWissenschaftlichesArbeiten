c5d1cd610eda6480ff3c25885e9666e9a7f170d2
Timo Stovermann <tsto@d-velop.de>
2022-10-14 13:56:55 +0200

    vue devserver

--- webclient/vue.config.js
+++ webclient/vue.config.js
@@ -2,5 +2,14 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    proxy: {
+      '/': {
+        target: 'http://localhost:8448/',
+        ws: false
+      }
+    }
+  },
+  outputDir: './assets/dist',
 })
