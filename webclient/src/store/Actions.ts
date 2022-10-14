bafc43f6871795a04adc6ce3118564998d4f6625
Timo Stovermann <tsto@d-velop.de>
2022-10-14 19:33:05 +0200

    getting appdata

--- webclient/src/store/Actions.ts
+++ webclient/src/store/Actions.ts
@@ -1,8 +1,7 @@
 import {MyState} from "@/store/MyState";
 import {ActionTree} from "vuex";
 import ActionTypes from "@/store/ActionTypes";
-import ReadAppData from "@/api/app/AppData";
-import MutationTypes from "@/store/MutationTypes";
+
 import {GetAppData} from "@/store/actions/appActions";
 
 export const actions: ActionTree<MyState, MyState> = {
