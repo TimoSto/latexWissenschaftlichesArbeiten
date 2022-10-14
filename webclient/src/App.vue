5a5b72ae41e697aa9f89066c891822a785cf730f
Timo Stovermann <tsto@d-velop.de>
2022-10-14 12:52:25 +0200

    homeview

--- webclient/src/App.vue
+++ webclient/src/App.vue
@@ -50,3 +50,60 @@ export default Vue.extend({
   }
 }
 </style>
+
+<style lang="scss">
+$dark-bg: #121212;
+
+.theme--dark.v-expansion-panels .v-expansion-panel:not(.keep){
+  background-color: $dark-bg;
+}
+.theme--dark.v-list:not(.keep) {
+  background-color: $dark-bg;
+}
+.theme--dark.v-sheet:not(.keep) {
+  background-color: $dark-bg;
+}
+
+.theme--dark.v-sheet.v-card:not(.v-sheet--outlined) {
+  box-shadow: 0px 3px 1px -2px rgba(255,255,255, 0.2), 0px 2px 2px 0px rgba(255, 255, 255, 0.14), 0px 1px 5px 0px rgba(255, 255, 255, 0.12);
+}
+
+//TODO: warum funktioniert elevate in home etc. nicht???/Warum ist main-bereich nach ohen scroll-verschiebbar
+.content-below-two-bars {
+  height: calc(100vh - 112px);
+  overflow-y: auto;
+
+  @media screen and (max-width: 959px) {
+    height: calc(100vh - 105px)!important;
+  }
+}
+
+.content-below-one-bar {
+  overflow-y: auto;
+  height: calc(100vh - 64px);
+
+  @media screen and (max-width: 959px) {
+    height: calc(100vh - 56px);
+  }
+}
+
+.v-toolbar__title {
+  padding-right: 4px;
+}
+
+.two-col-table {
+  & tr {
+    & td:first-child {
+      width: 150px;
+    }
+  }
+}
+
+.centered {
+  padding: 0 8px;
+  & .v-data-table {
+    margin: 0 auto;
+    border: 1px solid rgba(128,128,128, 0.25);;
+  }
+}
+</style>
