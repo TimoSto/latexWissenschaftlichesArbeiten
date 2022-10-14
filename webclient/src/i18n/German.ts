a0f56a787fe92c6906fc216e458dd11c8090da70
Timo Stovermann <tsto@d-velop.de>
2022-10-14 13:55:12 +0200

    wording

--- webclient/src/i18n/German.ts
+++ webclient/src/i18n/German.ts
@@ -5,11 +5,11 @@ export const GermanTranslations: TranslationKeys = {
         Welcome: 'Willkommen bei ThesorTeX!',
         Description: 'Dieses Tool erleichtert den Umgang mit LaTeX in verschiedenen Punkten.',
         TemplateCard: {
-            Title: 'Vorlage',
+            Title: 'Vorlage Projekt',
             Content: 'Die mit diesem Tool gelieferte Vorlage eignet sich für Ausarbeitungen mit wissenschaftlichem Anspruch. Aspekte wie Kopf- und Fußzeile, Inhaltsverzeichnis, Abbildungen, Tabellen und Literatur sowie Zitate sind abgedeckt. Klicke auf dieses Element, um sie herunterzuladen. Schau in die dazugehörige Dokumentation, um mehr zu erfahren.'
         },
         BibCard: {
-            Title: 'Literatur-Projekte',
+            Title: 'Projekte',
             Content: 'In der Vorlage wird ein eigenes System für die Literatur genutzt. Darin kannst du selbst auswählen, welche Attribute im Literaturverzeichnis und in Zitaten wie dargestellt werden sollen. Die Einträge kannst du über diese Anwendung hinzufügen. Ein Import aus Citavi kann ebenfalls aktiviert werden. Die Einträge kannst du in deinem TeX-Dokument über einen Befehl zitieren. Schau am besten mal in der Dokumentation unter dem Kapitel <i>Literatur</i>'
         },
         CVCard: {
