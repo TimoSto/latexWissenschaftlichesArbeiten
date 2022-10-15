5469fcb2ff0c707007d48216460162eadd2b14bb
Timo Stovermann <tsto@d-velop.de>
2022-10-15 11:45:25 +0200

    test title and sidebar enabling

--- e2e-tests/cypress/e2e/app/appTests.cy.ts
+++ e2e-tests/cypress/e2e/app/appTests.cy.ts
@@ -1,14 +1,51 @@
 /// <reference types="cypress" />
 
 import {VisitUrl} from "../../helpers/Browser";
-import {AssertTopAppBarHasText} from "../../helpers/AppAssertions.js";
+import * as App from "../../helpers/AppAssertions.js";
 
 describe('Global app-stuff', ()=> {
-    beforeEach(()=> {
-        VisitUrl('/#/');
-    });
+    describe('on home-page', () => {
+        beforeEach(()=> {
+            VisitUrl('/#/');
+        });
 
-    it('Title is "ThesorTeX"', ()=> {
-        AssertTopAppBarHasText('ThesorTeX');
-    });
+        it('Title is "ThesorTeX"', ()=> {
+            App.AssertTopAppBarHasText('ThesorTeX');
+        });
+
+        it('Sidebar is disabled', () => {
+            App.AssertNavIconIsDisabled();
+        });
+
+        it('Sidebar is closed', () => {
+            App.AssertSidebarClosed();
+        })
+
+        it('Sidebar is empty', () => {
+            App.AssertSidebarEmpty();
+        })
+    })
+
+    describe('on projects-page', () => {
+        beforeEach(()=> {
+            VisitUrl('/#/projects');
+        });
+
+        it('Title is "ThesorTeX - Projekte"', ()=> {
+            App.AssertTopAppBarHasText('ThesorTeX - Projekte');
+        });
+
+        it('Sidebar is not disabled', () => {
+            App.AssertNavIconIsNotDisabled();
+        });
+
+        it('Sidebar is closed', () => {
+            App.AssertSidebarClosed();
+        })
+
+        it('Sidebar is not empty', () => {
+            App.AssertSidebarNotEmpty();
+        })
+    })
+
 })
--- e2e-tests/cypress/helpers/AppAssertions.js
+++ e2e-tests/cypress/helpers/AppAssertions.js
@@ -2,3 +2,23 @@
 export function AssertTopAppBarHasText(text) {
     cy.get('header:first').find('.v-toolbar__title').should('have.text', text)
 }
+
+export function AssertNavIconIsDisabled() {
+    cy.get('.v-app-bar__nav-icon').should('be.disabled')
+}
+
+export function AssertNavIconIsNotDisabled() {
+    cy.get('.v-app-bar__nav-icon').should('not.be.disabled')
+}
+
+export function AssertSidebarClosed() {
+    cy.get('.v-navigation-drawer').should('have.class', 'v-navigation-drawer--mini-variant')
+}
+
+export function AssertSidebarEmpty() {
+    cy.get('.v-navigation-drawer__content').should('be.empty');
+}
+
+export function AssertSidebarNotEmpty() {
+    cy.get('.v-navigation-drawer__content').should('not.be.empty');
+}
