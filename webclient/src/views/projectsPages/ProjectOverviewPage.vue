237a2560e14f2fd009e69453b9f9bc5f8bb00adf
Timo Stovermann <tsto@d-velop.de>
2022-10-16 10:01:17 +0200

    project overview appbar

--- webclient/src/views/projectsPages/ProjectOverviewPage.vue
+++ webclient/src/views/projectsPages/ProjectOverviewPage.vue
@@ -1,13 +1,38 @@
 <template>
   <div id="projectOverviewPage">
+    <v-app-bar
+      dense
+      color="background"
+      elevate-on-scroll
+      >
+      <v-toolbar-title style="font-style: italic">{{projectTitle}}</v-toolbar-title>
 
+      <v-spacer />
+
+      <v-btn icon>
+        <v-icon>mdi-content-save</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-sheet class="content-below-two-bars">
+      hallo
+    </v-sheet>
   </div>
 </template>
 
-<script>
-export default {
-  name: "ProjectOverviewPage"
-}
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "ProjectOverviewPage",
+
+  computed: {
+
+    projectTitle() {
+      return this.$store.state.ProjectView.CurrentProject;
+    }
+
+  }
+})
 </script>
 
 <style scoped>
