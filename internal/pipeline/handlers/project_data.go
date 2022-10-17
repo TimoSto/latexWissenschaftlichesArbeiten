800da86cdfff99b1f1f71a4ba9e4c3bc21407176
Timo Stovermann <tsto@d-velop.de>
2022-10-17 17:02:22 +0200

    writing entries seems to work (empty)

--- internal/pipeline/handlers/project_data.go
+++ internal/pipeline/handlers/project_data.go
@@ -1,10 +1,17 @@
 package handlers
 
 import (
-	"fmt"
+	"ThesorTeX/internal/entries"
+	"ThesorTeX/pkg/logger"
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
+
+type ProjectData struct {
+	BibEntries []entries.BibEntry
+}
 
 func HandleGetProjectData(w http.ResponseWriter, r *http.Request) {
 	keys, ok := r.URL.Query()["project"]
@@ -17,5 +24,28 @@ func HandleGetProjectData(w http.ResponseWriter, r *http.Request) {
 
 	project := keys[0]
 
-	fmt.Println(project)
+	entries, err := entries.ReadBibEntries(project, ioutil.ReadFile)
+	if err != nil {
+		logger.LogError("Reading entries", err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	data := ProjectData{
+		BibEntries: entries,
+	}
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		logger.LogError("Marshaling data", err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	_, err = w.Write(jsonData)
+	if err != nil {
+		logger.LogError("Writing data", err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 }
