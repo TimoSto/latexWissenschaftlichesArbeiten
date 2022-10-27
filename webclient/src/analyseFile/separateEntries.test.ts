acbde3c8aa3f9f87a075a2f1c0dd926c90ada0ff
Timo Stovermann <tsto@d-velop.de>
2022-10-27 11:39:35 +0200

    separate entries test

--- webclient/src/analyseFile/separateEntries.test.ts
+++ webclient/src/analyseFile/separateEntries.test.ts
@@ -3,35 +3,35 @@ import separateEntries from "@/analyseFile/separateEntries";
 describe('separateEntries', () => {
     it('should give one', () => {
         const file = `
-        @book{
-        field1="hallo",
-        field2="@test"
-        }`;
+@book{
+field1="hallo",
+field2="@test"
+}`;
         expect(separateEntries(file)).toHaveLength(1);
     })
     it('should give two', () => {
         const file = `
-        @book{
-        field1="hallo",
-        field2="@test"
-        }
-        @wer{
-        field1="werlo",
-        field2="werest"
-        }`;
-        expect(separateEntries(file)).toHaveLength(1);
+@book{
+field1="hallo",
+field2="@test"
+}
+@wer{
+field1="werlo",
+field2="werest"
+}`;
+        expect(separateEntries(file)).toHaveLength(2);
     })
     it('should give two', () => {
         const file = `
-        @book{
-            field1="hallo",
-            field2="@test"
-        }
-        
-        @wer{
-        field1="werlo",
-        field2="werest"
-        }`;
-        expect(separateEntries(file)).toHaveLength(1);
+@book{
+    field1="hallo",
+    field2="@test"
+}
+
+@wer{
+field1="werlo",
+field2="werest"
+}`;
+        expect(separateEntries(file)).toHaveLength(2);
     })
 })
