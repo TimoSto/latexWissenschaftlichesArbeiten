dddee4993108a33bfef1f56cbc75ca0c89977df7
Timo Stovermann <tsto@d-velop.de>
2022-10-27 11:45:54 +0200

    repalcing indents

--- webclient/src/analyseFile/separateEntries.ts
+++ webclient/src/analyseFile/separateEntries.ts
@@ -2,5 +2,7 @@
 export default function separateEntries(file: string): string[] {
     file = file.substring(file.indexOf('@'))
 
+    file = file.replace(/\t/g, '');
+
     return file.split(/^@/gm).filter(e => e.length > 0);
 }
