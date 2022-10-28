05e2279ea85a789006e2fd0ea96d277c8a5f5f83
Timo Stovermann <tsto@d-velop.de>
2022-10-28 15:38:37 +0200

    Basic setup of reveal.js in projects-info (nothing shown yet but no error)

---
+++ webclient/src/components/RevealJsContainer.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="reveal">
+      <div class="slides">
+        <section data-markdown="" data-separator="^\n\n\n" data-separator-vertical="^\n\n">
+        </section>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import Reveal from 'reveal.js';
+import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+import Highlight from 'reveal.js/plugin/highlight/highlight.esm.js';
+import Notes from 'reveal.js/plugin/notes/notes.esm.js';
+
+export default Vue.extend({
+  name: "RevealJsContainer",
+  mounted() {
+    Reveal.initialize({
+      // used to keep current slide during browser reload
+      // fragment: true,
+      // fragmentInURL: true
+      controls: true,
+      progress: true,
+      center: true,
+
+      transition: 'slide', // none/fade/slide/convex/concave/zoom
+      transitionSpeed: 'fast',
+      totalTime: 15 * 60,
+
+      slideNumber: 'c/t',
+
+      // IMPORTANT: The order matters!
+      // So, RevealHightlight must be the LAST to load
+      plugins: [
+        Markdown, Notes, Highlight,
+      ]
+    })
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
--- webclient/src/views/projectsPages/ProjectInfoPage.vue
+++ webclient/src/views/projectsPages/ProjectInfoPage.vue
@@ -1,14 +1,17 @@
 <template>
   <div style="padding: 8px 16px" id="projectInfoPage">
     <p v-html="$t(i18nDictionary.Projects.Info.Description)"></p>
+    <RevealJsContainer />
   </div>
 </template>
 
 <script>
 import {i18nDictionary} from "@/i18n/Keys";
+import RevealJsContainer from "@/components/RevealJsContainer";
 
 export default {
   name: "ProjectInfoPage",
+  components: {RevealJsContainer},
   data() {
     return {
       i18nDictionary: i18nDictionary
