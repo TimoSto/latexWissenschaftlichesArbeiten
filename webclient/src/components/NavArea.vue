0c95e2a6a2c1837ae1d920ce466400f775d44ef3
Timo Stovermann <tsto@d-velop.de>
2022-10-31 15:18:47 +0100

    flex pages area with always 100% covered and only showisng the one(s) with class opened

--- webclient/src/components/NavArea.vue
+++ webclient/src/components/NavArea.vue
@@ -1,10 +1,15 @@
 <template>
-  <div style="display: flex;" :class="layoutClasses">
+  <div class="container" :class="layoutClasses">
 
-    <div class="pages">
+    <div class="pages" style="display: none">
       <div v-for="i in pages" :key="`page-${i}`" :id="`page-${i}`">
         <slot :name="`page-${i}`"></slot>
       </div>
+    </div>
+
+    <div class="pages">
+      <div id="page-1" style="background-color: red" class="opened"></div>
+      <div id="page-2" style="background-color: blue"></div>
     </div>
 
     <div class="edit-area">
@@ -37,51 +42,81 @@ export default Vue.extend({
 })
 </script>
 
-<style scoped lang="scss">
-.edit-area {
-  flex-grow: 1;
-  min-width: 0;
-  overflow: hidden;//TODO: shadow on right side if scrolled in overview and editor is open
-}
-
-.pages, .edit-area {
-  transition: width .25s ease-in;
-}
-
-.pages {
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  height: 100%;
   width: 100%;
-  box-sizing: border-box;
-}
-
-.theme--light .pages {
-  border-right: 1px solid rgba(0,0,0,0.1);
-}
-
-.theme--dark .pages {
-  border-right: 1px solid rgba(255,255,255,0.1);
-}
-
-.edit-area {
-  width: 0;
-  z-index: 100;
-}
-
-.two-thirds {
+  padding: 0;
   & .pages {
-    width: 30%;
-  }
-  & .edit-area {
-    width: 70%;
+    width: 100%;
+    display: flex;
+    flex: 1 1 auto;
+    box-sizing: border-box;
+    & [id^=page-] {
+      flex: 1 0 auto;
+      height: 100%;
+      width: 0;
+      transition: width .25s ease-in-out;
+      &.opened {
+        width: 100%;
+      }
+    }
   }
 }
-
-.half-screen {
-  & .pages {
-    width: 50%;
-  }
-  & .edit-area {
-    width: 50%;
-  }
-}
-
 </style>
+
+<!--<style scoped lang="scss">-->
+<!--.edit-area {-->
+<!--  flex-grow: 1;-->
+<!--  min-width: 0;-->
+<!--  overflow: hidden;//TODO: shadow on right side if scrolled in overview and editor is open-->
+<!--}-->
+
+<!--.pages, .edit-area {-->
+<!--  transition: width .25s ease-in;-->
+<!--}-->
+
+<!--.pages {-->
+<!--  width: 100%;-->
+<!--  box-sizing: border-box;-->
+<!--  display: flex;-->
+<!--  flex: 1 1 auto;-->
+<!--  & [id^=page-] {-->
+<!--    flex: 1 0 auto;-->
+<!--    height: 100%;-->
+<!--  }-->
+<!--}-->
+
+<!--.theme&#45;&#45;light .pages {-->
+<!--  border-right: 1px solid rgba(0,0,0,0.1);-->
+<!--}-->
+
+<!--.theme&#45;&#45;dark .pages {-->
+<!--  border-right: 1px solid rgba(255,255,255,0.1);-->
+<!--}-->
+
+<!--.edit-area {-->
+<!--  width: 0;-->
+<!--  z-index: 100;-->
+<!--}-->
+
+<!--.two-thirds {-->
+<!--  & .pages {-->
+<!--    width: 30%;-->
+<!--  }-->
+<!--  & .edit-area {-->
+<!--    width: 70%;-->
+<!--  }-->
+<!--}-->
+
+<!--.half-screen {-->
+<!--  & .pages {-->
+<!--    width: 50%;-->
+<!--  }-->
+<!--  & .edit-area {-->
+<!--    width: 50%;-->
+<!--  }-->
+<!--}-->
+
+<!--</style>-->
