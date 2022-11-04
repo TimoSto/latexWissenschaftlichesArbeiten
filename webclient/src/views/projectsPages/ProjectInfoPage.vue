5ce2511c8a55ac7221319164fd508bfeb1a2f808
Timo Stovermann <tsto@d-velop.de>
2022-11-04 13:41:11 +0100

    play-btn for reveal.js

--- webclient/src/views/projectsPages/ProjectInfoPage.vue
+++ webclient/src/views/projectsPages/ProjectInfoPage.vue
@@ -1,8 +1,13 @@
 <template>
   <div style="padding: 8px 16px" id="projectInfoPage">
     <p v-if="!revealJSAvailable">{{$t(i18nDictionary.Projects.Info.Description)}}</p>
-    <div class="presentation-frame">
-      <iframe ref="presentation" v-if="revealJSAvailable"></iframe>
+    <div class="presentation-frame" v-if="revealJSAvailable">
+      <div v-if="!started" style="position: absolute; top: calc(50% - 150px); left: calc(50% - 150px);">
+        <v-btn icon style="width: 300px; height: 300px;" @click="started=true" color="primary">
+          <v-icon size="300">mdi-play</v-icon>
+        </v-btn>
+      </div>
+      <iframe ref="presentation" v-if="started"></iframe>
     </div>
   </div>
 </template>
@@ -17,20 +22,23 @@ export default Vue.extend({
     return {
       i18nDictionary: i18nDictionary,
       revealJSAvailable: true,
+      started: false
     }
   },
-  mounted() {
-    this.$nextTick(() => {
-      const frame = this.$refs.presentation as HTMLIFrameElement;
-      frame.addEventListener('load', () => {
-        if( frame.contentDocument && !frame.contentDocument.body.classList.contains('reveal-viewport') ) {
-          this.revealJSAvailable = false;
+  watch: {
+    started() {
+      this.$nextTick(() => {
+        const frame = this.$refs.presentation as HTMLIFrameElement;
+        frame.addEventListener('load', () => {
+          if( frame.contentDocument && !frame.contentDocument.body.classList.contains('reveal-viewport') ) {
+            this.revealJSAvailable = false;
+          }
+        });
+        if( frame && frame.contentWindow ) {
+          frame.contentWindow.location.href = '/tutorials/reveal_js/presentation.html?presentation=projects&lang=' + this.$i18n.locale;
         }
-      });
-      if( frame && frame.contentWindow ) {
-        frame.contentWindow.location.href = '/tutorials/reveal_js/presentation.html?presentation=projects&lang=' + this.$i18n.locale;
-      }
-    })
+      })
+    }
   }
 })
 </script>
@@ -43,6 +51,7 @@ export default Vue.extend({
   max-height: calc(100vh - 200px);
   border: 2px solid var(--v-accent-lighten4);
   margin: 0 auto;
+  position: relative;
   & iframe {
     box-shadow: none;
     border: none;
